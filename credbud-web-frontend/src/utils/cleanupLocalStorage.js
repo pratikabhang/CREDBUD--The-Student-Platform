8a472059d0630daed475eb6961092db778071a37
PRATIK ABHANG <pratikabhang1@gmail.com>
2025-06-01 16:24:37 +0530

    credbud

--- credbud-web-frontend/src/utils/cleanupLocalStorage.js
+++
@@ -1,9 +0,0 @@
-export const cleanupLocalStorage = () => {
-  const now = new Date();
-
-  const item = JSON.parse(localStorage.userProfile);
-
-  if (item.expiry && now.getTime() > item.expiry) {
-    localStorage.removeItem("userProfile");
-  }
-};
